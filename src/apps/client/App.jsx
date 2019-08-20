320d436aab8f5d9855f406fe16799cc15a9148d6
annats <serozhkohanna@gmail.com>
2019-08-21 02:37:58 +0300

    разметка aboutUs

--- src/apps/client/App.jsx
+++ src/apps/client/App.jsx
@@ -10,6 +10,7 @@ import '../../css/main.css';
 import Header from './ui/components/Header/Header.jsx';
 import Footer from './ui/components/Footer/Footer.jsx';
 import MainPage from './ui/pages/MainPage/MainPage.jsx';
+import AboutPage from './ui/pages/AboutPage/AboutPage.jsx';
 
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
@@ -57,6 +58,7 @@ class App extends Component {
                 <div className={styles.pageContent}>
                     <Switch>
                         <Route exact path='/:lang(en)?' render={this.renderComponent(MainPage)} />
+                        <Route exact path='/about:lang(en)?' render={this.renderComponent(AboutPage)} />
                     </Switch>
                 </div>
                 <Footer />
---
+++ src/apps/client/ui/components/About/About.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import propOr from '@tinkoff/utils/object/propOr';
+import styles from './About.css';
+
+import { connect } from 'react-redux';
+
+const mapStateToProps = ({ application }) => {
+    return {
+        langMap: application.langMap
+    };
+};
+
+class About extends Component {
+    static propTypes = {
+        langMap: PropTypes.object.isRequired
+    };
+
+    render () {
+        const { langMap } = this.props;
+        const text = propOr('about', {}, langMap);
+
+        const content = [
+            {
+                img: '/src/apps/client/ui/components/About/files/can.png',
+                ...text.content[0]
+            },
+            {
+                img: '/src/apps/client/ui/components/About/files/bottle.png',
+                ...text.content[1]
+            },
+            {
+                img: '/src/apps/client/ui/components/About/files/can.png',
+                ...text.content[2]
+            }
+        ];
+
+        console.log(content);
+        return <div className={styles.about}>
+            <div className={styles.wrapper}>
+                <h1 className={styles.title}>{text.title}</h1>
+                <div className={styles.content}>
+                    {content.map((item, i) => {
+                        return (
+                            <div key={i} className={styles.contentBlock}>
+                                <img src={item.img} className={styles.img} />
+                                <p className={styles.description}>{item.text}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>;
+    }
+}
+
+export default connect(mapStateToProps)(About);
