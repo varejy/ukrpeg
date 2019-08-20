c03bdda4f812f5611d8a3876181a6d90764d863c
annats <serozhkohanna@gmail.com>
2019-08-20 15:05:28 +0300

    правки с input

--- src/apps/client/ui/components/Content/Content.jsx
+++ src/apps/client/ui/components/Content/Content.jsx
@@ -3,18 +3,25 @@ import PropTypes from 'prop-types';
 
 import propOr from '@tinkoff/utils/object/propOr';
 import styles from './Content.css';
+import { Link } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
 const mapStateToProps = ({ application }) => {
     return {
-        langMap: application.langMap
+        langMap: application.langMap,
+        langRoute: application.langRoute
     };
 };
 
 class Content extends Component {
     static propTypes = {
-        langMap: PropTypes.object.isRequired
+        langMap: PropTypes.object.isRequired,
+        langRoute: PropTypes.string
+    };
+
+    static defaultProps = {
+        langRoute: ''
     };
 
     state = {
@@ -35,13 +42,19 @@ class Content extends Component {
     }
 
     handleBlurInput = e => {
-        this.setState({
-            inputZoom: false
-        });
+        if (e.target.value !== '') {
+            this.setState({
+                inputZoom: true
+            });
+        } else {
+            this.setState({
+                inputZoom: false
+            });
+        }
     }
 
     render () {
-        const { langMap } = this.props;
+        const { langMap, langRoute } = this.props;
         const { inputZoom, inputValue } = this.state;
         const text = propOr('content', {}, langMap);
 
@@ -49,12 +62,12 @@ class Content extends Component {
             <div className={styles.wrapper}>
                 <div className={styles.photoBlock}>
                     <div className={styles.topBlock}>
-                        <div className={styles.moreBtn}>
+                        <Link to={`${langRoute}/rvv`} className={styles.moreBtn}>
                             <p className={styles.arrowBtn}>
                                 <img src='/src/apps/client/ui/components/Content/files/arrow.png' className={styles.arrowImg} />
                             </p>
                             <div className={styles.btn}>{text.buttonText}</div>
-                        </div>
+                        </Link>
                         <div className={styles.mainText}>
                             <h1 className={styles.title}>{text.title}</h1>
                             <hr className={styles.horizontalLine} />
