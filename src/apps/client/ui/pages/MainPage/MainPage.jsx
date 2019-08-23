9df8b1d05b63ab45ad73369bb5ac5faff17e3b71
VictoriaVM <victoria.v.movchan@gmail.com>
2019-08-23 10:29:28 +0300

    Правки верстки

--- src/apps/client/ui/pages/MainPage/MainPage.jsx
+++ src/apps/client/ui/pages/MainPage/MainPage.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Content from '../../components/Content/Content';
 import Companies from '../../components/Companies/Companies';
 import Articles from '../../components/Articles/Articles';
+import styles from './MainPage.css';
 
 class MainPage extends Component {
     render () {
-        return <section>
+        return <section className={styles.mainPageContainer}>
+            <div className={styles.gridContainer}>
+                <div className={styles.column1}/>
+                <div className={styles.column2}/>
+                <div className={styles.column3}/>
+                <div className={styles.column4}/>
+                <div className={styles.column5}/>
+            </div>
             <Content />
             <Companies />
             <Articles />
