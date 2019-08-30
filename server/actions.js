c6df0390752f8d01b3eea18fcd4e94b923f149b4
annats <serozhkohanna@gmail.com>
2019-08-30 18:46:33 +0300

    добавление контента через админку ("законодавства")

--- server/actions.js
+++ server/actions.js
@@ -1,7 +1,9 @@
 import getMobileDetect from '../src/apps/client/services/server/getMobileDetect';
 import getLangFromRoute from '../src/apps/client/services/server/getLangFromRoute';
+import getLaws from '../src/apps/client/services/server/getLaws';
 
 export default [
     getMobileDetect,
-    getLangFromRoute
+    getLangFromRoute,
+    getLaws
 ];
--- src/apps/admin/reducers/application.js
+++ src/apps/admin/reducers/application.js
@@ -1,11 +1,13 @@
 import {
     SET_AUTHENTICATED,
-    SET_CATEGORIES
+    SET_CATEGORIES,
+    SET_LAW
 } from '../types/types';
 
 const initialState = {
     authenticated: null,
-    newsCategories: []
+    newsCategories: [],
+    lawList: []
 };
 
 export default function (state = initialState, action) {
@@ -14,6 +16,8 @@ export default function (state = initialState, action) {
         return { ...state, authenticated: action.payload };
     case SET_CATEGORIES:
         return { ...state, newsCategories: action.payload };
+    case SET_LAW:
+        return { ...state, lawList: action.payload };
     default:
         return state;
     }
