99f2643eac019938e2496cd63b3cb7a12db6cd99
annats <serozhkohanna@gmail.com>
2019-09-02 09:55:21 +0300

    правки

--- src/apps/admin/reducers/index.js
+++ src/apps/admin/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
 import application from './application';
-import laws from './laws';
 import news from './news';
 
 const reducers = combineReducers({
     application,
-    laws,
     news
 });
 
