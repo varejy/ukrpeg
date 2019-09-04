016ac9fe3a4aace75482fc4bc60a7fa75b3b91df
VictoriaVM <victoria.v.movchan@gmail.com>
2019-09-04 12:26:14 +0300

    Сетка на Контактах и Законодательстве

--- src/apps/client/ui/pages/ContactPage/ContactPage.jsx
+++ src/apps/client/ui/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import Contact from '../../components/Contact/Contact';
+import styles from './ContactPage.css';
+import classNames from 'classnames';
 
 class ContactPage extends Component {
     render () {
-        return <section>
+        return <section className={styles.contactPageContainer}>
+            <div className={styles.gridContainer}>
+                <div className={classNames(styles.column, styles.column1)}/>
+                <div className={classNames(styles.column, styles.column2)}/>
+                <div className={classNames(styles.column, styles.column3)}/>
+                <div className={classNames(styles.column, styles.column4)}/>
+                <div className={classNames(styles.column, styles.column5)}/>
+            </div>
             <Contact />
         </section>;
     }
