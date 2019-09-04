91e085b940786e641a26e7c9ff64e2ef994f72c8
annats <serozhkohanna@gmail.com>
2019-09-04 18:14:20 +0300

    поиск новостей

--- src/apps/admin/ui/pages/SeoPage/SeoPage.jsx
+++ src/apps/admin/ui/pages/SeoPage/SeoPage.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import SwipeableViews from 'react-swipeable-views';
 
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import pick from '@tinkoff/utils/object/pick';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
 import classNames from 'classnames';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import find from '@tinkoff/utils/array/find';
 
 import SeoTabs from '../../components/SeoTabs/SeoTabs.jsx';
 import getAllSeo from '../../../services/getAllSeo';
+import getCategories from '../../../services/getCategories';
+import search from '../../../services/search';
+import MetaForm from '../../components/MetaForm/MetaForm.jsx';
 
 const materialStyles = () => ({
     paper: {
@@ -79,33 +92,109 @@ const PAGES = [
     { header: 'Законодавства', page: 'contacts' },
     { header: 'Контакти', page: 'contacts' }
 ];
+const mapStateToProps = ({ application, news }) => {
+    return {
+        categories: application.newsCategories,
+        news: news.news
+    };
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    getAllSeo: payload => dispatch(getAllSeo(payload))
+    getAllSeo: payload => dispatch(getAllSeo(payload)),
+    getCategories: payload => dispatch(getCategories(payload)),
+    search: payload => dispatch(search(payload))
 });
 
 class SeoPage extends Component {
   static propTypes = {
       classes: PropTypes.object.isRequired,
-      getAllSeo: PropTypes.func.isRequired
+      getAllSeo: PropTypes.func.isRequired,
+      search: PropTypes.func,
+      news: PropTypes.array,
+      categories: PropTypes.array,
+      getCategories: PropTypes.func
   };
 
-  constructor (...args) {
-      super(...args);
+    static defaultProps = {
+        news: [],
+        categories: []
+    };
 
-      this.state = {
-          loading: true
-      };
-  }
+    constructor (...args) {
+        super(...args);
 
-  componentDidMount () {
-      this.props.getAllSeo()
-          .then(() => {
-              this.setState({
-                  loading: false
-              });
-          });
-  }
+        this.state = {
+            loading: true,
+            tabsValue: 0,
+            tips: [],
+            searchTxt: '',
+            selectedProduct: null
+        };
+    }
+
+    componentWillReceiveProps (nextProps) {
+        if (this.props.news !== nextProps.news) {
+            this.setTips(nextProps);
+        }
+    }
+
+    componentDidMount () {
+        this.props.getAllSeo()
+            .then(() => {
+                this.setState({
+                    loading: false
+                });
+            });
+        this.props.getCategories();
+    }
+
+    handleTableChange = event => () => {
+        this.setState({
+            tabsValue: event
+        });
+    };
+
+    handleSearchChange = event => {
+        const searchTxt = event.target.value;
+
+        this.setState({
+            searchTxt
+        });
+
+        searchTxt.length !== 0
+            ? this.searchProducts(searchTxt)
+            : this.setState({
+                tips: [],
+                selectedProduct: {}
+            });
+    };
+
+    handleSelectedProduct = product => () => {
+        this.setState({
+            selectedProduct: product
+        });
+    };
+
+    searchProducts = searchText => {
+        this.props.search(searchText);
+    };
+
+    setTips = (props = this.props) => {
+        const { selectedProduct } = this.state;
+        const { news } = props;
+        const newTips = news
+            .map(product => {
+                return {
+                    ...product
+                };
+            });
+        const newSelectedProduct = selectedProduct ? find(tip => tip.id === selectedProduct.id, newTips) : null;
+
+        this.setState({
+            tips: newTips,
+            selectedProduct: newSelectedProduct
+        });
+    };
 
     renderEditPagesSeo = () => {
         const { classes } = this.props;
@@ -120,20 +209,107 @@ class SeoPage extends Component {
         </div>;
     };
 
+    renderEditCategoriesSeo = () => {
+        const { classes, categories } = this.props;
+        let categoriesMap = [];
+
+        categories.map(category => {
+            categoriesMap.push({ header: `Редактирование категории '${category.name}'`, page: category.name });
+        });
+
+        return <div>
+            <Paper className={classes.paper}>
+                <div className={classes.headerContainer}>
+                    <Typography variant='h6' id='seoTitle'>SEO</Typography>
+                </div>
+                <SeoTabs pages={categoriesMap} categories={categories} option='category'/>
+            </Paper>
+        </div>;
+    };
+
+    renderSearchPage = () => {
+        const { classes } = this.props;
+        const { searchTxt, tips, selectedProduct } = this.state;
+        const { id } = pick(['id'], selectedProduct || {});
+        const check = (prop) => id === prop;
+
+        return <div>
+            <div className={classes.searchWrapp}>
+                <TextField
+                    label='Поиск'
+                    value={searchTxt}
+                    onChange={this.handleSearchChange}
+                    margin='normal'
+                    className={classes.search}
+                    variant='outlined'
+                />
+            </div>
+            {
+                searchTxt.length !== 0 && <div className={classes.products}>
+                    <Typography variant='h6'>{` По запросу '${searchTxt}' ${(!tips.length && searchTxt.length) ? 'ничего не найдено' : 'найдено'}`}</Typography>
+                    <div className={classes.cardContainer}>
+                        {
+                            tips.map(tip => {
+                                return <div key={tip.id} onClick={this.handleSelectedProduct(tip)} className={classes.cardLink}>
+                                    <Card className={classNames(classes.card, { [classes.selectedProduct]: check(tip.id) })}>
+                                        <CardHeader
+                                            title={tip.name}
+                                            subheader={tip.company}
+                                        />
+                                        <CardMedia
+                                            className={classes.media}
+                                            image={tip.avatar}
+                                            title={tip.name}
+                                        />
+                                    </Card>
+                                </div>;
+                            })
+                        }
+                    </div>
+                </div>
+            }
+            {
+                !!id && <Paper className={classes.paper} component="div">
+                    <MetaForm page='product' product={selectedProduct} searchQuery={searchTxt} option='product'/>
+                </Paper>
+            }
+        </div>;
+    };
+
     render () {
         const { classes } = this.props;
-        const { loading } = this.state;
+        const { loading, tabsValue } = this.state;
 
         if (loading) {
             return <div className={classes.loader}>
                 <CircularProgress />
             </div>;
         }
-
+        console.log(this.props.news);
         return <div className={classes.container}>
-            {this.renderEditPagesSeo()}
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={tabsValue}
+                    onChange={this.handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                >
+                    <Tab onClick={this.handleTableChange(0)} label="Редактирование SEO страниц" />
+                    <Tab onClick={this.handleTableChange(1)} label="Редактирование SEO категорий" />
+                    <Tab onClick={this.handleTableChange(2)} label="Поиск по товарам" />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                index={tabsValue}
+                onChangeIndex={this.handleChangeIndex}
+            >
+                {this.renderEditPagesSeo(0)}
+                {this.renderEditCategoriesSeo(1)}
+                {this.renderSearchPage(2)}
+            </SwipeableViews>
         </div>;
     }
 }
 
-export default connect(null, mapDispatchToProps)(withStyles(materialStyles)(SeoPage));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(materialStyles)(SeoPage));
