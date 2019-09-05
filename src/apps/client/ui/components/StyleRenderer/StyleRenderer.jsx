a361870923277153f3b6dd608bb5cc58c40f2714
Volodymyr Bilous <v.bilous14@gmail.com>
2019-09-05 15:23:38 +0300

    Добавлен рендеринг инстаграм постов

--- src/apps/client/ui/components/StyleRenderer/StyleRenderer.jsx
+++ src/apps/client/ui/components/StyleRenderer/StyleRenderer.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import styles from './StyleRenderer.css';
 
+import getInstagramTemplate from './utils/getInstagramTemplate';
+
 export default class StyleRenderer extends Component {
     static propTypes = {
         html: PropTypes.string
@@ -10,6 +12,22 @@ export default class StyleRenderer extends Component {
 
     static defaultProps = {
         html: ''
+    };
+
+    componentDidMount () {
+        this.setInstagramPosts();
+    }
+
+    setInstagramPosts = () => {
+        document.querySelectorAll('oembed[url]').forEach(element => {
+            const url = element.getAttribute('url');
+
+            if (url.indexOf('https://www.instagram.com') === 0) {
+                element.innerHTML = getInstagramTemplate(url);
+            }
+        });
+
+        window.instgrm.Embeds.process();
     };
 
     render () {
