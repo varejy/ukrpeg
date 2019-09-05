6b95a8077e1fba3fc57e402b9c3ddfcb5adced4c
Volodymyr Bilous <v.bilous14@gmail.com>
2019-09-05 09:59:36 +0300

    Lint fix

--- src/apps/admin/constants/routes.js
+++ src/apps/admin/constants/routes.js
@@ -4,5 +4,5 @@ export default [
     { id: 'products', path: '/admin/products', exact: true, title: 'Товары' },
     { id: 'news', path: '/admin/news', exact: true, title: 'Новости' },
     { id: 'categories', path: '/admin/credentials', exact: true, title: 'Смена учетных данных', notMenu: true },
-    { id: 'seo', path: '/admin/seo', exact: true, title: 'SEO' },
+    { id: 'seo', path: '/admin/seo', exact: true, title: 'SEO' }
 ];
