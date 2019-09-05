90413f5c91688f188360ad900b891327496b8634
varejy <lionvasia@mail.ru>
2019-09-05 11:47:58 +0300

    bag fix

--- src/apps/admin/types/types.js
+++ src/apps/admin/types/types.js
@@ -3,5 +3,5 @@ export const // eslint-disable-line one-var
     SET_LAWS = 'SET_LAWS',
     SET_NEWS = 'SET_NEWS',
     SET_CATEGORIES = 'SET_CATEGORIES',
-    SET_RVV_PLANS = 'SET_RVV_PLANS';
+    SET_RVV_PLANS = 'SET_RVV_PLANS',
     SET_SEO = 'SET_SEO';
