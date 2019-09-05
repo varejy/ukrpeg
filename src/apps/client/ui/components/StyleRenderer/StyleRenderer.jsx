98f00a19389925396a2a5f64b7244e02d8c094d4
VictoriaVM <victoria.v.movchan@gmail.com>
2019-09-05 14:35:12 +0300

    Правки

--- src/apps/client/ui/components/StyleRenderer/StyleRenderer.jsx
+++ src/apps/client/ui/components/StyleRenderer/StyleRenderer.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import getInstagramTemplate from './utils/getInstagramTemplate';
-
 import styles from './StyleRenderer.css';
 
 export default class StyleRenderer extends Component {
