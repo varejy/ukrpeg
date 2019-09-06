fddace36691ef1526f89e2991a2cd28dbecb14b9
varejy <lionvasia@mail.ru>
2019-09-06 11:12:39 +0300

    temp

--- server/api/client/rvv/model.js
+++ server/api/client/rvv/model.js
@@ -4,31 +4,38 @@ const Schema = mongoose.Schema;
 
 const rvv = new Schema({
     id: { type: String, required: true },
-    info: {
-        texts: {
-            type: {
-                plans: [{
-                    type: String,
-                    required: true
-                }],
-                why: [
-                    {
-                        type: {
-                            text: {
-                                type: String,
-                                required: true
-                            },
-                            img: {
-                                type: String,
-                                required: true
-                            }
-                        },
+    texts: {
+        type: {
+            plans: [{
+                type: {
+                    name: {
+                        type: String,
+                        required: true
+                    },
+                    positionIndex: {
+                        type: String,
                         required: true
                     }
-                ]
-            },
-            required: true
-        }
+                },
+                required: true
+            }],
+            why: [
+                {
+                    type: {
+                        text: {
+                            type: String,
+                            required: true
+                        },
+                        img: {
+                            type: String,
+                            required: true
+                        }
+                    },
+                    required: true
+                }
+            ]
+        },
+        required: true
     }
 });
 
