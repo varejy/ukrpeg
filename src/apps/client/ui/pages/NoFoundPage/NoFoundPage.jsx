f8f0b8a2b6ed5c28bca5f083b5c52765eef83d43
annats <serozhkohanna@gmail.com>
2019-09-09 12:39:54 +0300

    правки

--- src/apps/client/ui/pages/NoFoundPage/NoFoundPage.jsx
+++ src/apps/client/ui/pages/NoFoundPage/NoFoundPage.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './NoFoundPage.css';
+import propOr from '@tinkoff/utils/object/propOr';
+import { connect } from 'react-redux';
 
-function NoFoundPage () {
-    return (
-        <div className={styles.noFound}>
-            <p>404</p>
-            <p>page not found</p>
-        </div>
-    );
+const mapStateToProps = ({ application }) => {
+    return {
+        langMap: application.langMap
+    };
+};
+
+class NoFoundPage extends Component {
+  static propTypes = {
+      langMap: PropTypes.object.isRequired
+  };
+
+  render () {
+      const { langMap } = this.props;
+      const text = propOr('noFoundPage', {}, langMap);
+
+      return (
+          <div className={styles.noFound}>
+              <p className={styles.text}>404</p>
+              <p>{text.desc}</p>
+          </div>
+      );
+  }
 }
-export default NoFoundPage;
+
+export default connect(mapStateToProps)(NoFoundPage);
