9fe07a3c100154de494446c5bb503615387cfa44
Volodymyr Bilous <v.bilous14@gmail.com>
2019-09-23 15:46:25 +0300

    New api key

--- src/apps/client/constants/constants.js
+++ src/apps/client/constants/constants.js
@@ -2,4 +2,4 @@ export const EN = 'en';
 export const UA = 'ua';
 export const DEFAULT_LANG = UA;
 export const DEFAULT_LANG_ROUTE = '/';
-export const GOOGLE_MAPS_KEY = 'AIzaSyBuoPeg7r1iH5SYtXyRMX_u5Izo5GG7Fc4';
+export const GOOGLE_MAPS_KEY = 'AIzaSyBz7wfcHWCcQe__bzOXEfKJKZ3r_LmVCH0';
