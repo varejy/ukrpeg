6ce6a6d40aae4a8a066bb4410acd77d6c79bad40
Volodymyr Bilous <v.bilous.upwork@gmail.com>
2019-11-21 15:14:28 +0200

    gogetssl verification

--- server/helpers/verification.js
+++ server/helpers/verification.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default function wwwRedirect (req, res, next) {
-    if (req.originalUrl.match(/^\/.well-known\/pki-validation\/247F40C458DA926F1493D88C3CE5DE8E.txt/)) {
+    if (req.originalUrl.match(/^\/.well-known\/pki-validation\/55DF69A746A2FA1057CFE15A57722598.txt/)) {
         return res.sendFile(path.resolve(__dirname, '..', 'verification', 'https.txt'));
     }
 
