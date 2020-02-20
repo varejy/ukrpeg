86bf1dbec82d523ca97214f3b86b745440f8332d
v.bilous <v.bilous14@gmail.com>
2020-02-20 19:33:19 +0200

    https verification

--- server/helpers/verification.js
+++ server/helpers/verification.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default function wwwRedirect (req, res, next) {
-    if (req.originalUrl.match(/^\/.well-known\/pki-validation\/55DF69A746A2FA1057CFE15A57722598.txt/)) {
+    if (req.originalUrl.match(/^\/.well-known\/pki-validation\/498CD1BF1F8ED54BFC7B0925B1C7DF4D.txt/)) {
         return res.sendFile(path.resolve(__dirname, '..', 'verification', 'https.txt'));
     }
 
